06d8ee664e17eb787aaffc80991e9a83b6f9baf0
Ashwith Godishala <ugs23179_it.ashwith@cbit.org.in>
2025-04-08 14:49:57 +0000

    step-4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%@(^(w-$af!70h^75wnn5=e&76rt9i8z!lm#x@9-!*2scc43(a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookish-bassoon-7vvjxxqv77ww3p794-8000.app.github.dev']
 
 
 # Application definition
